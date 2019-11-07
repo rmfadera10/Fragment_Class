8ebe60195f0c9fd4e37667757c552d72eaa5f787
rmfadera10 <55060246+rmfadera10@users.noreply.github.com>
2019-11-07 13:05:39 +0545

    Fragment-Sum of two number, Area of circle

--- app/src/main/java/fragment/FirstFragment.java
+++ app/src/main/java/fragment/FirstFragment.java
@@ -8,15 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.letstravel.fragment.MainActivity;
 import com.letstravel.fragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements View.OnClickListener{
 
-
+private Button btncalculate;
+private EditText txtfirst, txtsecond;
     public FirstFragment() {
         // Required empty public constructor
     }
@@ -26,7 +31,22 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view= inflater.inflate(R.layout.fragment_first, container, false);
+
+        txtfirst=view.findViewById(R.id.fistnumber);
+        txtsecond=view.findViewById(R.id.secondnumber);
+
+        btncalculate=view.findViewById(R.id.btncalculate);
+        btncalculate.setOnClickListener(this);
+        return view;
+
     }
 
+    @Override
+    public void onClick(View v) {
+        int first=Integer.parseInt(txtfirst.getText().toString());
+        int second=Integer.parseInt(txtfirst.getText().toString());
+        int result=first+second;
+        Toast.makeText(getActivity(), "Sum of two number is:" +result, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/fragment/SecondFragment.java
+++ app/src/main/java/fragment/SecondFragment.java
@@ -8,14 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.letstravel.fragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SecondFragment extends Fragment {
-
+public class SecondFragment extends Fragment implements View.OnClickListener {
+ private EditText txtradius;
+ private Button btnArea;
 
     public SecondFragment() {
         // Required empty public constructor
@@ -26,7 +30,18 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view= inflater.inflate(R.layout.fragment_second, container, false);
+
+        txtradius=view.findViewById(R.id.txtradius);
+        btnArea=view.findViewById(R.id.btnArea);
+        btnArea.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        int radiuss=Integer.parseInt(txtradius.getText().toString());
+        float area=3.14f*radiuss*radiuss;
+        Toast.makeText(getActivity(), "Area of Circle is" +area, Toast.LENGTH_SHORT).show();
+    }
 }
